65fd91f31276c9e72b29cf2065d025da1f3622cb
Ariane Madureira <arianemadureira@live.com>
2024-07-20 16:24:18 -0300

    add swagger

--- program.ts
+++ program.ts
@@ -1,10 +1,13 @@
-import Server  from "./server";
-import {MysqlDataBase} from "./external/MysqlDataBase";
+import Server from './server';
+import { MysqlDataBase } from './external/MysqlDataBase';
+
 let port = process.env.PORT || 3000;
 const db = new MysqlDataBase();
 const _server = new Server(db);
 
+// Swagger setup é chamado no server.ts
 
 _server.app.listen(port, () => {
-    console.log('Server exec: PORTA -> ' + port);
+    console.log('Server running on port ' + port);
+    console.log(`Swagger docs are available at http://localhost:${port}/api-docs`);
 });
